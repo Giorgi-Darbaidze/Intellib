62dfb9917145d87741c836d79dd317e675797aa4
NEMO <64556451+Giorgi-Darbaidze@users.noreply.github.com>
2023-02-18 22:38:22 +0400

    Update theme.js

--- JS/modules/theme.js
+++ JS/modules/theme.js
@@ -8,12 +8,13 @@ function dark_theme_function(){
 	discord_link.classList.add("our_dis_dark_theme")
 	discord_link.classList.add("our_dis_dark_theme:hover")
 	document.getElementById('header_theme').style.backgroundColor = 'rgba(25, 25, 112, 0.683'
-	document.getElementById('header_theme').style.color = 'white'
 	document.getElementById('list_theme').style.boxShadow = "0 -3rem 4rem rgb(33, 40, 57)"
 	document.getElementById('html_theme').style.backgroundColor = 'rgb(33, 40, 57)'
+	document.getElementById('themes_style').style.borderColor = 'white'
+	document.getElementById('header_theme').style.color = 'white'
 	document.getElementById('html_theme').style.color = 'white'
-	document.getElementById('bar').style.color = 'white'
 	document.getElementById('bar').style.borderColor = 'white'
+	document.getElementById('bar').style.color = 'white'
 	
 	even.forEach(even => {
 		even.style.backgroundColor = 'rgb(36, 44, 114)'
@@ -31,13 +32,14 @@ function dark_theme_function(){
 function light_theme_function(){
 	discord_link.classList.remove("our_dis_dark_theme")
 	discord_link.classList.add("our_dis")
-	document.getElementById('header_theme').style.backgroundColor = 'rgb(65, 105, 225)'
-	document.getElementById('header_theme').style.color = 'black'
 	document.getElementById('list_theme').style.boxShadow = "0 -3rem 4rem rgb(0, 191, 255)"
+	document.getElementById('header_theme').style.backgroundColor = 'rgb(65, 105, 225)'
 	document.getElementById('html_theme').style.backgroundColor = 'white'
+	document.getElementById('themes_style').style.borderColor = 'black'
+	document.getElementById('header_theme').style.color = 'black'
 	document.getElementById('html_theme').style.color = 'black'
-	document.getElementById('bar').style.color = 'black'
 	document.getElementById('bar').style.borderColor = 'black'
+	document.getElementById('bar').style.color = 'black'
 
 	even.forEach(even => {
 		even.style.backgroundColor = 'rgb(32, 170, 255)'
